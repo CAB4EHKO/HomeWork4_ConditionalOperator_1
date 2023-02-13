4e4a9546233e9aa16472a751980bda371a9ce648
Savchenko <savchenko.ra@yandex.ru>
2023-02-13 17:26:12 +0300

    Solution of the second task.

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
-        if ( age >= 18 ){
+        task1();
+        task2();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1.");
+        int age = 18;
+        if (age >= 18) {
             System.out.println("Если возраст человека равен: " + age + " , то он совершеннолетний.");
-        }
-        else {
+        } else {
             System.out.println("Если возраст человека равен: " + age +
                     " , то он не достиг совершеннолетия, нужно немного подождать.");
+        }
+    }
+    public static void task2() {
+        System.out.println("Задача 2.");
+        int temp = 4;
+        boolean isCold = temp < 5;
+        if (isCold) {
+            System.out.println("На улице " + temp + " градуса, нужно надеть шапку.");
+        }
+        else {
+            System.out.println("На улице " + temp + " градусов, можно идти без шапки.");
         }
     }
 }
